fb26a69a056b648e653802656ce8b275f04558ea
bartek8b <163062773+bartek8b@users.noreply.github.com>
2026-02-15 19:15:44 +0100

    Set basic layput of the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import { useState } from 'react'
-import './App.css'
+import { Form } from './components/Form';
+import { Preview } from './components/Preview';
 
 function App() {
-
-
   return (
     <>
-
+      <header>CV Builder</header>
+      <a href="#preview" style={{ textAlign: 'center' }}>
+        Scroll to Prewiev &#8681;
+      </a>
+      <main>
+        <Form />
+        <Preview />
+      </main>
+      <footer>
+        <a
+          href="https://github.com/bartek8b"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Created by &#128279; Bartlomiej Balcerzak
+        </a>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
