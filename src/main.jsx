228d9fe1fb81ec1dc8593cc5226bc7b12abb0e39
bartek8b <163062773+bartek8b@users.noreply.github.com>
2026-02-15 13:40:23 +0100

    Add '-min' to modern-normailize import name

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './modern-normalize.css'
+import './modern-normalize.min.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
