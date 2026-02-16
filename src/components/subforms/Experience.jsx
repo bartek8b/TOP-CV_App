6db35c06a7ced2d120a8b947499b9df1f6a2e399
bartek8b <163062773+bartek8b@users.noreply.github.com>
2026-02-16 23:46:38 +0100

    Set a11y on delete item buttons

--- src/components/subforms/Experience.jsx
+++ src/components/subforms/Experience.jsx
@@ -65,8 +65,11 @@ export function Experience({
             className="delete-btn"
             type="button"
             onClick={() => removeExperience(exp.id)}
+            aria-label={`Delete experience item ${index + 1}`}
           >
             <svg
+              aria-hidden="true"
+              focusable="false"
               xmlns="http://www.w3.org/2000/svg"
               width="24"
               height="24"
@@ -78,12 +81,12 @@ export function Experience({
               strokeLinejoin="round"
               className="feather feather-trash-2"
             >
-              <polyline points="3 6 5 6 21 6"></polyline>
-              <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-              <line x1="10" y1="11" x2="10" y2="17"></line>
-              <line x1="14" y1="11" x2="14" y2="17"></line>
-            </svg>{' '}
-            Item #{index + 1}
+              <polyline points="3 6 5 6 21 6" />
+              <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+              <line x1="10" y1="11" x2="10" y2="17" />
+              <line x1="14" y1="11" x2="14" y2="17" />
+            </svg>
+            <span aria-hidden="true">Item #{index + 1}</span>
           </button>
         </div>
       ))}
--- src/components/subforms/Interests.jsx
+++ src/components/subforms/Interests.jsx
@@ -43,8 +43,11 @@ export function Interests({
             className="delete-btn"
             type="button"
             onClick={() => removeInterest(i.id)}
+            aria-label={`Delete interest item ${index + 1}`}
           >
             <svg
+              aria-hidden="true"
+              focusable="false"
               xmlns="http://www.w3.org/2000/svg"
               width="24"
               height="24"
@@ -56,12 +59,12 @@ export function Interests({
               strokeLinejoin="round"
               className="feather feather-trash-2"
             >
-              <polyline points="3 6 5 6 21 6"></polyline>
-              <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-              <line x1="10" y1="11" x2="10" y2="17"></line>
-              <line x1="14" y1="11" x2="14" y2="17"></line>
-            </svg>{' '}
-            Item #{index + 1}
+              <polyline points="3 6 5 6 21 6" />
+              <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+              <line x1="10" y1="11" x2="10" y2="17" />
+              <line x1="14" y1="11" x2="14" y2="17" />
+            </svg>
+            <span aria-hidden="true">Item #{index + 1}</span>
           </button>
         </div>
       ))}
